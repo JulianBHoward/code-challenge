4bbb19b8db3055c02d253f585d6ef6629e839232
JulianBHoward <140655434+JulianBHoward@users.noreply.github.com>
2023-09-27 20:38:42 -0400

    Update jbh.js

--- jbh.js
+++ jbh.js
@@ -12,13 +12,13 @@ const findSum = function(array) {
   };
 
   const findFrequency = function (array) {
-    if (!arr || arr.length === 0) {
+    if (!array || array.length === 0) {
         return [null, null]; 
     }
 
     const frequencyMap = new Map();
 
-    arr.map(item => {
+    array.map(item => {
         const count = frequencyMap.get(item) || 0;
         frequencyMap.set(item, count + 1);
     });
@@ -37,21 +37,21 @@ const findSum = function(array) {
     });
 
     return [mostFrequentItem, leastFrequentItem];
-}
-const isPalindrome = function(str) {
-    for( var i = palindrom.length; i > 0; i-- )
+};
+const isPalindrome = function (palindrome) {
+    for( var i = palindrome.length; i > 0; i-- )
     {
-        if( palindrom[i] = palindrom.charAt(palindrom.length)-1 )
+        if( palindrome[i] = palindrome.charAt(palindrome.length)-1 )
         {
             return true
         }else{
            return false
         }
     }
-}
+};
   
 
-const largestPair = function(array){
+const largestPair = function(numbers){
     var numbers = [1, 3, 5, 8]
 var products = [];
 
@@ -72,26 +72,17 @@ for (var i=0; i < numbers.length; i++){
 var largestProduct = Math.max (...products);
 
 
-}
+};
 
-const removeParenth = function(str) {
+const removeParenth = function(newString) {
     let originalString = ('ido(not)liketocode');
     newString = originalString.replace('(','');
     newString = newString.replace(')','');
-    console.log(newString);
+    
+    return newString;
   };
 
-const scoreScrabble = function(str) {
-    let letterValue = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 },
-
-word = word.toLowerCase();
-for (i = 0; i < word.length; i++) {
-    sum += letterValue[word[i].toLowerCase] || 0; 
-}
-return sum;
-  };
-
-const scoreScrabble = function(str) {
+const scoreScrabble = function(scoreScrabble) {
     let letterValue = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 },
 
 word = word.toLowerCase();
